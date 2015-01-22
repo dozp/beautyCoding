9ff7b145ce85802a545ee13971a28c863d5150bd
dozp <dzp01@163.com>
2015-01-22 23:25:17 +0800

    part of finished

--- 1.3_PieSort/pie_sort.cc
+++ 1.3_PieSort/pie_sort.cc
@@ -8,51 +8,61 @@ int PieSort(vector<int> &ivec)
 	//for(auto i : ivec)
 	//	cout<< i<<" ";
 	//cout<<endl;
+	
 	int reverse_times=0;
-	vector<int>::iterator it=ivec.begin(), it_reverse_last=ivec.end();
-	vector<int>::iterator it_search_last=it_reverse_last;
-	vector<int>::iterator it_reverse_first=it;
-	//int cnt=0;
-	while(it<it_search_last)
+	// each pie is considered as an element in vector
+	// the bottom pie is v[0], larger index is at upper layer
+	// the indexes is divided into 3 parts
+	// 0, 1, 2,..., n1 | n2, n2+1, ..., n3 | n4,...n
+	// |--range1--------|-----range2--------|---range3--|
+	//  b0		     b1			 b2	     b3
+	// range1: in Descending order
+	// range2: unsorted
+	// range3: in ascending order
+	// for example: 10,9,8|0,4,3,2|6,7
+	// range1[i]>range3[j]>range2[k]
+	vector<int>::iterator b0, b1, b2, b3;
+	b0=b1=ivec.begin();
+	b2=b3=ivec.end();
+
+	while(b1<b2)
 	{
-		vector<int>::iterator max_it=max_element(it, it_search_last);
-		if(it_search_last-it==1)//there is only one element in the range
+		vector<int>::iterator r2_max_it=max_element(b1, b2);
+		if(r2_max_it==b1)	//max is at the head of r2
 		{
-			if(it_search_last!=ivec.end())//array is in ascending order
+			if(b2==b3)
 			{
-
-				reverse(it_reverse_first,ivec.end());
-				++reverse_times;
+				++b1;	//transplant max from r2 to r1
 			}
-			++it;
-		}
-		else if(max_it!=it_search_last-1) //max is not at the end of search range
-		{
-			if(it_search_last!=ivec.end())//there is  sorted tail part
-
+			else
 			{
-				reverse(it_reverse_first, ivec.end());//move sorted tail part to bottom
-				++reverse_times;
-				it_reverse_first+=(ivec.end()-it_search_last);
-				it=it_reverse_first;//set new search head	
-				it_search_last=ivec.end();//reset end of search range			
-			}
-			else if(max_it==it)//max is at the head of search range
-			{
-				++it_reverse_first;//the first ones are already in right position
-				++it;
-			}
-			else//max is within the search range
-			{
-				//move max to tail/(top of heap)
-				reverse(max_it, ivec.end());
-				++reverse_times;
+				reverse(b1, b3);
+				reverse_times++;
+				size_t r3_len=b3-b2;
+				b1+=r3_len;
+				b2=b3;	//empty r3
 			}
 		}
-		else if(max_it==it_search_last-1)//max is at the tail of search range, the tail part is already in ascending order
+		else if(r2_max_it==b2-1)	//max is at end
 		{
-			--it_search_last; 
+			// transplant max from r2 to r3
+			--b2;
 		}
+		else if(b2<b3)	// r3 is not empty
+		{
+			// reverse r3 to r1
+			reverse(b1, b3);
+			reverse_times++;
+			size_t r3_len=b3-b2;
+			b1+=r3_len;
+			b2=b3;	//empty r3
+		}
+	}
+	if(b0==b1-1)	//only one element in r1
+	{
+		//reverse r3
+		reverse(b2,b3);
+		++reverse_times;
 	}
 	//cout<<"reverse times:"<<reverse_times<<endl;
 	//cout<<"Output vector:";
--- 1.3_PieSort/pie_sort_unittest.cc
+++ 1.3_PieSort/pie_sort_unittest.cc
@@ -1,12 +1,39 @@
 #include "pie_sort.h"
 #include "gtest/gtest.h"
 using namespace std;
+TEST(PieSortTest, OneTimeReserse)
+{
+	int act_time=0, exp_time=1;
+	vector<int> ivec={1,2,3,4,5};
+	vector<int> exp_ivec={5,4,3,2,1};
+	act_time=PieSort(ivec);
+	EXPECT_EQ(exp_ivec,ivec);
+	EXPECT_EQ(exp_time,act_time);
+}
 TEST(PieSortTest, NoReverse)
 {
 	int act_time=0, exp_time=0;
-	vector<int> ivec={1,2,3,4,5};
-	vector<int> exp_ivec={1,2,3,4,5};
+	vector<int> ivec={5,4,3,2,1};
+	vector<int> exp_ivec={5,4,3,2,1};
 	act_time=PieSort(ivec);
-	ASSERT_TRUE(ivec==exp_ivec);
-	EXPECT_EQ(act_time, exp_time);
+	EXPECT_EQ(exp_ivec,ivec);
+	EXPECT_EQ(exp_time,act_time);
+}
+TEST(PieSortTest, test1)
+{
+	int act_time=0, exp_time=1;
+	vector<int> ivec={5,4,3,1,2};
+	vector<int> exp_ivec={5,4,3,2,1};
+	act_time=PieSort(ivec);
+	EXPECT_EQ(exp_ivec,ivec);
+	EXPECT_EQ(exp_time,act_time);
+}
+TEST(PieSortTest, test2)
+{
+	int act_time=0, exp_time=2;
+	vector<int> ivec={1,2,3,5,4};
+	vector<int> exp_ivec={5,4,3,2,1};
+	act_time=PieSort(ivec);
+	EXPECT_EQ(exp_ivec,ivec);
+	EXPECT_EQ(exp_time,act_time);
 }
